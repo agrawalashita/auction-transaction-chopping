51aa8347c4eae6c5f8404db11649e06dd1dd6633
kcottur <kcottur@uci.edu>
2024-06-06 00:58:32 -0700

    fix perf log

--- nodes/server.py
+++ nodes/server.py
@@ -114,7 +114,7 @@ def on_message(ws, message):
 
     print("Running perceived latency (s):", total_perceived_latency)
     print("Running actual latency (s):", total_actual_latency)
-    print("Running throughput (hops/us):", num_hops / total_actual_latency)
+    print("Running throughput (hops/ms):", num_hops / total_actual_latency)
 
 def on_error(ws, error):
     print(error)
